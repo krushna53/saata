c9732f0eae2fbb163ca69bd733be2f14510c7a4c
vinod <keshariyavinod793@gmail.com>
2026-01-29 20:15:57 +0530

    MLL certificate update image url

--- src/Components/MLL2026.jsx
+++ src/Components/MLL2026.jsx
@@ -4,7 +4,7 @@ const MLL2026 = () => {
   return (
     <CertificateForm
       csvFile="/certificateName-2026.csv"
-      bgImage="https://images.ctfassets.net/acxjtojz8lp2/22jOBXzf4k0F0q4LxluEDP/4d9ab962ebbd49fd0a1935a85ab7c4fd/SATAA_certificate-2.jpg"
+      bgImage="https://images.ctfassets.net/acxjtojz8lp2/22jOBXzf4k0F0q4LxluEDP/f485bc61254114adaea5c4e4497ed2d7/SATAA_certificate-4__2_.jpg"
       title="Generate Your MLL 2026 Certificate"
     />
   );
